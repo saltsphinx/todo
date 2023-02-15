8dc1f13af184f0dad78a12dc5e469fb8562f3cea
tremar thomas <81628964+saltsphinx@users.noreply.github.com>
2023-02-15 05:54:00 -0600

    Add toggleTodo method, add parameter to Add method projectFactory

--- src/projectFactory.js
+++ src/projectFactory.js
@@ -1,12 +1,33 @@
 function ProjectFactory(name, id)
 {
   const todos = [];
-  const add = (todo) =>
+  const add = (todo, index) =>
   {
-    const tierIndex = this.todos.indexOf(x => x.tier == todo.tier);
+    let tierIndex = index ? index : this.todos.indexOf(x => x.tier == todo.tier);
     tierIndex == 0 ? todos.unshift(todo) : this._addTodo(todo, tierIndex);
 
     return tierIndex;
+  }
+
+  const toggleTodo = (todoId) =>
+  {
+    const todo = this.todos.splice(todos.findIndex(x => x.id == todoId), 1)[0];
+    todo.toggle();
+
+    const tierIndex = this.todos.indexOf(x => x.tier == todo.tier);
+
+    if (todo.active)
+    {
+      this.add(todo, tierIndex);
+      return tierIndex;
+    }
+    else
+    {
+      const inactiveIndex = this.todos.indexOf(x => x.active == false);
+      _addTodo(todo, inactiveIndex);
+
+      return inactiveIndex;
+    }
   }
 
   const _addTodo = (todo, index) =>
