630f43b2a1757def436a60eef7c2fb333467cb13
tremar thomas <81628964+saltsphinx@users.noreply.github.com>
2023-02-16 07:46:22 -0600

    Make methods and params more syntactically agnostic projectHandler

--- src/projectHandler.js
+++ src/projectHandler.js
@@ -28,18 +28,23 @@ const removeProject = (projectName) =>
   projects.splice(projectIndex, 1);
 }
 
-const addTodo = (projectName, name, description, dueDate, tier) =>
+const addTodo = (projectName, todoName, description, dueDate, tier) =>
 {
   const projectIndex = _checkName(projectName);
 
-  if (!projectIndex)
+  if (projectIndex === false)
   {
     return false;
   }
 
   const project = projects[projectIndex];
 
-  const todo = todoFactory(name, description, dueDate, tier);
+  if (project.todos.length > 0 && typeof _checkName(todoName, project.todos) == 'number')
+  {
+    return false;
+  }
+
+  const todo = todoFactory(todoName, description, dueDate, tier);
   return project.add(todo);
 }
 
@@ -48,10 +53,10 @@ const removeTodo = (projectID, todoID) =>
 
 }
 
-function _checkName(projectName, arr = projects)
+function _checkName(name, arr = projects)
 {
   const names = projects.map(project => project.name);
-  const index = names.findIndex(name => name == projectName);
+  const index = names.findIndex(x => x == name);
   console.log(index);
 
   if (index >= 0)
