d875c473f04df77b971a73075d59799eb52c876c
tremar thomas <81628964+saltsphinx@users.noreply.github.com>
2023-02-24 18:34:55 -0600

    Create projectView module

---
+++ src/projectView.js
@@ -0,0 +1,37 @@
+const $ = require('jquery');
+let selected;
+window.$ = $;
+
+// create project node for view, returns node
+function createProjectTemplate(name)
+{
+  const projectNode = $(`<div class='project' id=${name}>`);
+  const p = $('<p>');
+  p.text(name);
+  
+  projectNode.append(p);
+  projectNode.appendTo($('.projects'));
+  projectNode.on('click', select);
+
+  return projectNode[0];
+}
+
+function select(event)
+{
+  const target = event.currentTarget;
+  loadView(target);
+}
+
+function loadView(node)
+{
+  console.log(node);
+  if (selected)
+  {
+    selected.classList.remove('selected');
+  }
+
+  selected = node;
+  node.classList.add('selected');
+}
+
+module.exports = { createProjectTemplate, loadView};
